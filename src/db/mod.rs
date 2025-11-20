acc906a6232b761fa7bb569516948853e168702d
yihiruyetsughpod <242951239+yihiruyetsughpod@users.noreply.github.com>
2025-11-20 01:19:24 +0000

    Create mod.rs
    - Add core functionality

---
+++ src/db/mod.rs
@@ -0,0 +1,14 @@
+mod queries;
+
+pub mod models;
+
+pub use queries::*;
+
+pub type DbPool = sqlx::postgres::PgPool;
+pub type RollupStatusType = i16;
+
+pub mod table_name {
+    pub const BATCH: &str = "batch";
+    pub const BLOCK: &str = "l2_block";
+    pub const CHUNK: &str = "chunk";
+}
